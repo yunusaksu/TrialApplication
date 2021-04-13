f12171e5f943ff0beafbd0b84cc60db40ba7cd0b
yunusaksu <dev.yunus.aksu@gmail.com>
2021-04-13 17:28:59 +0300

    Shiraz's first commit

--- TrialApplication/Form1.Designer.cs
+++ TrialApplication/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TrialApplication
 {
-    partial class Form1
+    partial class FormAnasayfa
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(286, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HELLO WORLD!";
+            // 
+            // FormAnasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "FormAnasayfa";
+            this.Text = "Anasayfa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TrialApplication/Form1.cs
+++ TrialApplication/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TrialApplication
 {
-    public partial class Form1 : Form
+    public partial class FormAnasayfa : Form
     {
-        public Form1()
+        public FormAnasayfa()
         {
             InitializeComponent();
         }
